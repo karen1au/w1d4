449b9a68f1c617458c0da0054a10c49a849d9329
karen1au <kagemikaka@hotmail.com>
2019-03-07 16:14:25 +0000

    Print the index of Waldo when found

---
+++ find-waldo.js
@@ -0,0 +1,15 @@
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(idx) {
+  console.log("Found Waldo at index "+idx+"!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// Found Waldo at index 2!
