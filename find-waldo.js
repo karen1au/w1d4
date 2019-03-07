92d3fe044118a06bf42bad84a506b6b7cbf791d3
karen1au <kagemikaka@hotmail.com>
2019-03-07 16:17:42 +0000

    refactor with forEach method

--- find-waldo.js
+++ find-waldo.js
@@ -1,9 +1,9 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(name => {
+    if (name === "Waldo") {
+      found(arr.indexOf(name));   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(idx) {
